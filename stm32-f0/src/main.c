abc1af1b707cc5f5450e08194e8dae07bd63acf2
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-03-25 22:27:45 +0200

    [stm32-f0] Adapt code to work with stm32f042 and stm32f051.

--- stm32-f0/src/main.c
+++ stm32-f0/src/main.c
@@ -1,9 +1,19 @@
-#ifdef STM32F0
 #include "stm32f0xx.h"
+
+#ifdef STM32F051x8
 #define LEDPORT (GPIOC)
-#define LED1 (13)
 #define ENABLE_GPIO_CLOCK (RCC->AHBENR |= RCC_AHBENR_GPIOCEN)
-#define GPIOMODER (GPIO_CRH_MODE13_0)
+// Set GPIOx_MODER (MODER8, 16 and 17 bits) for PC8 to 01 (general purpose output mode)
+#define GPIOMODER (GPIO_MODER_MODER8_0)
+#define GPIO_SETTER (GPIO_BSRR_BS_8)
+#define GPIO_RESETTER (GPIO_BSRR_BR_8)
+#elif STM32F042x6
+#define LEDPORT (GPIOA)
+#define ENABLE_GPIO_CLOCK (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+// Set GPIOx_MODER (MODER4, 8 and 9 bits) for PA4 to 01 (general purpose output mode)
+#define GPIOMODER (GPIO_MODER_MODER4_0)
+#define GPIO_SETTER (GPIO_BSRR_BS_4)
+#define GPIO_RESETTER (GPIO_BSRR_BR_4)
 #endif
 
 void ms_delay(int ms)
@@ -15,17 +25,17 @@ void ms_delay(int ms)
   }
 }
 
-//Alternates blue and green LEDs quickly
 int main(void)
 {
-  ENABLE_GPIO_CLOCK; 		 					// enable the clock to GPIO
-  GPIOC->MODER |= (1 << 16);
+  ENABLE_GPIO_CLOCK;
+
+  LEDPORT->MODER |= GPIOMODER;
 
   for (;;) {
-    GPIOC->BSRR = (1 << 8);
-    ms_delay(100);
-    GPIOC->BRR = (1 << 8);
-    ms_delay(100);
+    LEDPORT->BSRR = GPIO_SETTER;
+    ms_delay(1000);
+    LEDPORT->BSRR = GPIO_RESETTER;
+    ms_delay(1000);
   }
 
   return 0;
