8df844db657c521bd1f1b0ac8059ff008f36b95e
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-04-02 23:04:10 +0200

    Add example with GPIO interrupt in C.

--- stm32-f0/src/main.c
+++ stm32-f0/src/main.c
@@ -16,27 +16,44 @@
 #define GPIO_RESETTER (GPIO_BSRR_BR_4)
 #endif
 
-void ms_delay(int ms)
-{
-  while (ms-- > 0) {
-    volatile int x=500;
-    while (x-- > 0)
-        __asm("nop");
-  }
-}
+volatile int EXTI0Flag = 0;
 
-int main(void)
-{
+int main(void) {
+  // Reset EXTI0 to 0x0000
+  SYSCFG->EXTICR[0] &= ~SYSCFG_EXTICR1_EXTI0;
+  // Enable for PA0
+  SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA;
+
+  // Configure PA0 to trigger an interrupt event on the EXTI0 line on a rising edge.
+  EXTI->RTSR |= EXTI_RTSR_TR0;
+
+  // Unmask the external interrupt line EXTI0 by setting the bit corresponding to the EXTI0 "bit 0" in the EXT_IMR register.
+  EXTI->IMR |= EXTI_IMR_MR0;
+
+  // Set priority for the `EXTI0` line to `1`.
+  NVIC_SetPriority(EXTI0_1_IRQn, 1);
+
+  // Enable the interrupt in the NVIC.
+  NVIC_EnableIRQ(EXTI0_1_IRQn);
+
   ENABLE_GPIO_CLOCK;
 
   LEDPORT->MODER |= GPIOMODER;
 
   for (;;) {
-    LEDPORT->BSRR = GPIO_SETTER;
-    ms_delay(1000);
-    LEDPORT->BSRR = GPIO_RESETTER;
-    ms_delay(1000);
   }
 
   return 0;
+}
+
+// See http://www.hertaville.com/external-interrupts-on-the-stm32f0.html.
+void EXTI0_1_IRQHandler(void) {
+  if ((EXTI->IMR & EXTI_IMR_MR0) && (EXTI->PR & EXTI_PR_PR0)) {
+    while (GPIOA->IDR & GPIO_IDR_0) {}
+
+    EXTI0Flag = EXTI0Flag == 0 ? 1 : 0;
+    LEDPORT->BSRR = EXTI0Flag == 1 ? GPIO_SETTER : GPIO_RESETTER;
+
+    EXTI->PR |= EXTI_PR_PR0;
+  }
 }
