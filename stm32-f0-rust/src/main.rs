f38bea64a522a980b31953551fa805441398d70d
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-04-25 21:19:34 +0200

    Simplify borrowing of peripherals.

--- stm32-f0-rust/src/main.rs
+++ stm32-f0-rust/src/main.rs
@@ -14,6 +14,7 @@ extern crate stm32f0x1 as stm32f0x;
 #[cfg(feature = "stm32f042")]
 #[macro_use(interrupt)]
 extern crate stm32f0x2 as stm32f0x;*/
+
 #[macro_use(interrupt)]
 extern crate stm32f0x1 as stm32f0x;
 
@@ -35,7 +36,7 @@ use stm32f0x::Peripherals;
 
 use beeper::Beeper;
 use button::Button;
-use rtc::{RTC, Time};
+use rtc::{Time, RTC};
 
 static CORE_PERIPHERALS: Mutex<RefCell<Option<CorePeripherals>>> = Mutex::new(RefCell::new(None));
 static PERIPHERALS: Mutex<RefCell<Option<Peripherals>>> = Mutex::new(RefCell::new(None));
@@ -51,44 +52,37 @@ fn main() {
         *CORE_PERIPHERALS.borrow(cs).borrow_mut() = Some(cortex_m::Peripherals::take().unwrap());
     });
 
-    interrupt::free(|cs| {
-        if let (Some(mut cp), Some(p)) = (
-            CORE_PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
-            PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
-        ) {
-            Beeper::configure(&p);
-            writeln!(stdout, "Beeper configured.").unwrap();
+    interrupt_free(|mut cp, p| {
+        Beeper::configure(&p);
+        writeln!(stdout, "Beeper configured.").unwrap();
 
-            {
-                Button::configure(&p, &mut cp);
-                writeln!(stdout, "Button configured.").unwrap();
-            }
+        Button::configure(&p, &mut cp);
+        writeln!(stdout, "Button configured.").unwrap();
 
-            {
-                let mut rtc = RTC::new(&mut cp, &p);
-                rtc.configure();
+        {
+            let mut rtc = RTC::new(&mut cp, &p);
+            rtc.configure();
 
-                // Configure alarm.
-                rtc.configure_alarm(&Time {
-                    hours: 17,
-                    minutes: 58,
-                    seconds: 15,
-                });
+            // Configure alarm.
+            rtc.configure_alarm(&Time {
+                hours: 17,
+                minutes: 58,
+                seconds: 15,
+            });
 
-                // Set time.
-                rtc.configure_time(&Time {
-                    hours: 17,
-                    minutes: 58,
-                    seconds: 10,
-                });
+            // Set time.
+            rtc.configure_time(&Time {
+                hours: 17,
+                minutes: 58,
+                seconds: 10,
+            });
 
-                rtc.toggle_alarm(false);
+            rtc.toggle_alarm(false);
 
-                writeln!(stdout, "RTC configured: {:?}", rtc.get_time()).unwrap();
-            }
-
-            configure_standby_mode(&cp, p);
+            writeln!(stdout, "RTC configured: {:?}", rtc.get_time()).unwrap();
         }
+
+        configure_standby_mode(&cp, p);
     });
 
     loop {
@@ -115,30 +109,25 @@ fn on_alarm() {
     let mut stdout = hio::hstdout().unwrap();
     writeln!(stdout, "Alarm interrupt!").unwrap();
 
-    interrupt::free(|cs| {
-        if let (Some(mut cp), Some(p)) = (
-            CORE_PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
-            PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
-        ) {
-            play_melody(Beeper::new(&mut cp, p));
+    interrupt_free(|mut cp, p| {
+        play_melody(Beeper::new(&mut cp, p));
 
-            let mut rtc = RTC::new(&mut cp, &p);
+        let mut rtc = RTC::new(&mut cp, &p);
 
-            // Check alarm A flag.
-            if rtc.is_alarm_interrupt() {
-                let mut current_time = rtc.get_time();
+        // Check alarm A flag.
+        if rtc.is_alarm_interrupt() {
+            let mut current_time = rtc.get_time();
 
-                writeln!(stdout, "Clear pending... {:?}", current_time).unwrap();
+            writeln!(stdout, "Clear pending... {:?}", current_time).unwrap();
 
-                current_time.add_seconds(10);
+            current_time.add_seconds(10);
 
-                rtc.configure_alarm(&current_time);
+            rtc.configure_alarm(&current_time);
 
-                rtc.clear_pending_interrupt();
-            } else {
-                writeln!(stdout, "Disabling...").unwrap();
-                rtc.disable_interrupt();
-            }
+            rtc.clear_pending_interrupt();
+        } else {
+            writeln!(stdout, "Disabling...").unwrap();
+            rtc.disable_interrupt();
         }
     });
 }
@@ -148,26 +137,35 @@ interrupt!(EXTI0_1, button_handler);
 fn button_handler() {
     let mut stdout = hio::hstdout().unwrap();
 
-    interrupt::free(|cs| {
-        if let (Some(mut cp), Some(p)) = (
-            CORE_PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
-            PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
-        ) {
-            play_melody(Beeper::new(&mut cp, p));
-            writeln!(
-                stdout,
-                "Button interrupt, button is pressed: {:?}!",
-                p.GPIOA.idr.read().idr0().bit_is_set()
-            ).unwrap();
+    interrupt_free(|mut cp, p| {
+        play_melody(Beeper::new(&mut cp, p));
+        writeln!(
+            stdout,
+            "Button interrupt, button is pressed: {:?}!",
+            p.GPIOA.idr.read().idr0().bit_is_set()
+        ).unwrap();
 
-            // Set pending register to mark interrupt as handled.
-            p.EXTI.pr.modify(|_, w| w.pr0().set_bit());
-            // Clear Wakeup flag.
-            p.PWR.cr.modify(|_, w| w.cwuf().set_bit());
-        }
+        // Set pending register to mark interrupt as handled.
+        p.EXTI.pr.modify(|_, w| w.pr0().set_bit());
+        // Clear Wakeup flag.
+        p.PWR.cr.modify(|_, w| w.cwuf().set_bit());
     });
 }
 
 fn play_melody(mut beeper: Beeper) {
     beeper.play_melody();
+}
+
+fn interrupt_free<F>(f: F) -> ()
+where
+    F: FnOnce(&mut CorePeripherals, &Peripherals),
+{
+    interrupt::free(|cs| {
+        if let (Some(mut cp), Some(p)) = (
+            CORE_PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
+            PERIPHERALS.borrow(cs).borrow_mut().as_mut(),
+        ) {
+            f(cp, p);
+        }
+    });
 }
